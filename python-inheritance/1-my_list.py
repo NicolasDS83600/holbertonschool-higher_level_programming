9fee7f70df0170e5d03b1eb43014ed9294b6302a
NicolasDS83600 <nico836001@outlook.fr>
2026-02-04 09:44:18 +0100

    refactor: rename class Mylist to MyList in 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Module that defines a Mylist class with a sorted print method."""
 
 
-class Mylist(list):
+class MyList(list):
     """Custom list class which print its elements in sorted order."""
 
     def print_sorted(self):
