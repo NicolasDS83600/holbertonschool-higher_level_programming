592f5b848217465ee9fe41a016d52c8279c6c5d4
NicolasDS83600 <nico836001@outlook.fr>
2026-04-03 12:58:16 +0200

    refactor:use mutable default argument to persist iteration state

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string():
-    magic_string.iteration = getattr(magic_string, "iteration", 0) + 1
-    return ", ".join(["BestSchool"] * magic_string.iteration)
+def magic_string(iteration=[0]):
+    iteration[0] += 1
+    return ", ".join(["BestSchool"] * iteration[0])
